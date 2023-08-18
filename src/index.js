f87c3eceba226998e41e98d6e2049ae6627645f6
javier <sientendieras@hotmail.com>
2023-08-18 13:08:31 -0500

    v.01

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
 /* modal-products */
 
-const openModal = document.querySelector('.products__info-addone');
-const modal = document.querySelector('.productsmodalone');
-const closeModal = document.querySelector('.productsmodalone__closeone');
+const openModal = document.querySelector('.products__info-addone', '.products__info-addtwo');
+const modal = document.querySelector('.productsmodalone','.productsmodaltwo');
+const closeModal = document.querySelector('.productsmodalone__closeone','.productsmodalone__closetwo');
 
   openModal.addEventListener('click', (e)=>{
     e.preventDefault();
-    modal.classList.add('productsmodalone--show');
+    modal.classList.add('productsmodalone--show','productsmodaltwo--show' );
   });
 
   closeModal.addEventListener('click', (e)=>{
     e.preventDefault();
-    modal.classList.remove('productsmodalone--show');
+    modal.classList.remove('productsmodalone--show','productsmodaltwo--show');
   });
 
-
-
-/* const openModal = document.querySelector('.products__info-addtwo');
-const modal = document.querySelector('.productsmodaltwo');
-const closeModal = document.querySelector('.productsmodalone__closetwo');
-
-  openModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.add('productsmodaltwo--show');
-  });
-
-  closeModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('productsmodaltwo--show');
-  }); */
-
-
-/* modal*/
