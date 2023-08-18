6b70fae324ec693c72fcc68c28017dfe28d103ba
javier <sientendieras@hotmail.com>
2023-08-18 18:25:23 -0500

    terminado-javiecon modal

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 
-const cowOpenModal= document.querySelector('.hero-cta');
-const modal= document.querySelector('.modal-cow');
-const closeModal= document.querySelector('.modal-cow__close');
-
-const close1 =document.querySelector('.close');
-
-cowOpenModal.addEventListener('click',(e)=>{
-e.preventDefault();
-modal.classList.add('modal-cow__show')
-});
+import {} from './js/modalcow';
 
 
-closeModal.addEventListener('click',(e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal-cow__show')
-    });
-    
-    close1.addEventListener('click',(e)=>{
-        e.preventDefault();
-        modal.classList.remove('modal-cow__show')
-
-    });
-
-    
